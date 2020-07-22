448439d950ffa8ccbbe2a38bd258492fa6d99f3b
Aishah <aissshah@outlook.com>
2020-07-22 17:44:59 +0100

    Add different urls

--- harvard-api.js
+++ harvard-api.js
@@ -1,23 +1,28 @@
+//initialising variables
 const startURL = "https://api.harvardartmuseums.org/object";
-let 
+const apiKey = "?apikey=664fe862-4dac-4184-8c4a-3aa1620d459c";
+let title = "sky";
+let classification = "Paintings";
+let finalURL = startURL + apiKey + "&title=" + title + "&classification=" + classification;
 
-
-fetch("https://api.harvardartmuseums.org/classification/26?apikey=664fe862-4dac-4184-8c4a-3aa1620d459c")
-    .then(response => response.json())
-    .then(json => console.log(json))
+fetch(finalURL)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status)
+        };
+        return response.json()
+    })
+    .then(json => console.log(json.records[2].primaryimageurl))
     .catch(console.error)
 
-var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
-var queryString = $.param({
-        apikey: "664fe862-4dac-4184-8c4a-3aa1620d459c",
-        title: "rabbit",
-        classification: "Paintings"
-    });
-    
-    $.getJSON(apiEndpointBaseURL + "?" + queryString, function(data) {
-       console.log(data); 
-    });
+    //how many results: json.info.totalrecords
+    //image url: json.records[0].primaryimageurl
+    //title:
+    //artist:
+    //century:
 
-console.log(queryString);
-
+ //list of different classifications
+ //if statements to make the search broader if not specificied
+ //if statements for when no result is found
+ //if statement for when no image is found
 //apikey=664fe862-4dac-4184-8c4a-3aa1620d459c&title=rabbit&classification=Paintings
