3bd146cdd60bbf318b183d2d80c96546bb92dd17
Aishah <aissshah@outlook.com>
2020-07-23 11:37:41 +0100

    Add functions to check image availability
    The checkImageAvailable function and checkRecordsAvailable function to check for pictures and loop through.

--- harvard-api.js
+++ harvard-api.js
@@ -4,6 +4,9 @@ const apiKey = "?apikey=664fe862-4dac-4184-8c4a-3aa1620d459c";
 let title = "fire";
 let classification = "Paintings";
 let finalURL = startURL + apiKey + "&title=" + title + "&classification=" + classification;
+let imageSRC, century, artistName, artworkName;
+let randomNumber;
+let checkLoop = false;
 
 fetch(finalURL)
     .then(response => {
@@ -14,27 +17,53 @@ fetch(finalURL)
     })
     .then(json => {
         console.log(json);
-        console.log(json.records[2].century);
-        console.log(json.records[2].title);
-        console.log(json.records[2].primaryimageurl);
-        console.log(json.records[2].people[0].name);
         
+        randomNumber = Math.floor(Math.random() * json.records.length);
+        checkIfRecordsAvailable(json);
+        checkImageAvailable(json);
+        console.log(century, artworkName, artistName)
+        console.log(imageSRC);
     })
     .catch(console.error)
 
+//to see if no records
+const checkIfRecordsAvailable = data => {
+    if (data.records.length === 0) {
+        noImageAvailable();
+    }
+}
+
+//another option is to start the loop from 0 but then loop might run forever
+const checkImageAvailable = data => {
+    while(!data.records[randomNumber].primaryimageurl){
+        randomNumber++;
+        if (randomNumber >= data.records.length) {
+            if (checkLoop) {
+                noImageAvailable();
+            } else {
+                randomNumber = 0;
+                checkLoop = true;
+            }
+        }
+    }
+    imageSRC = data.records[randomNumber].primaryimageurl;
+    artworkName = data.records[randomNumber].title;
+    century = data.records[randomNumber].century;
+
+    if (data.records[randomNumber].peoplecount !== 0) {
+        artistName = data.records[randomNumber].people[0].name;
+    } else {
+        artistName = "Unknown";
+    }
+}
+
+//this function will return when no image is there and manipulate dom saying that. also stop other functions from running?
+const noImageAvailable = () => {
+
+}
 
 
-    // n = Math.floor(Math.random * json.records.length)
-
-    //how many results: json.info.totalrecords or json.records.length
-    //image url: json.records[0].primaryimageurl
-    //title: json.records[0].title
-    //artist: if json.records[0].peoplecount === 0; else, json.records[0].people[0].name
-    //century: json.records[0].century
-    
 //how to hide API key
- //list of different classifications
- //if statements to make the search broader if not specificied
- //if statements for when no result is found
- //if statement for when no image is found if imagecount = 0
 //apikey=664fe862-4dac-4184-8c4a-3aa1620d459c&title=rabbit&classification=Paintings
+//list of different classifications
+//stretch: if statements to make the search broader if not specificied
